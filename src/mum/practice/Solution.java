0a5e0655d9cb5f2bd252ac93fd7eea8bbd861388
Khanh Tran Cong <trancongkhanh@gmail.com>
2017-08-12 16:22:35 +0700

    mum

--- src/mum/practice/Solution.java
+++ src/mum/practice/Solution.java
@@ -1,10 +1,30 @@
 package mum.practice;
 
-public abstract class Solution {
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class Solution {
 	protected Object input;
-	protected Object output;	
-	public abstract Object solve();
-	public void printResult() {
+	protected Object output;
+	protected Scanner sc;
+	public Solution() {
+		
+	}
+	public Object solve() {
+		sc = new Scanner(new ByteArrayInputStream(input.toString().getBytes()));
+		return null;
+	}
+
+	public void printResult() {		
 		System.out.println(output);
+	}
+
+	public int[] readArray() {
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		return a;
 	}
 }
